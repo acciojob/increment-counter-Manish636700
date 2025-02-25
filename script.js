8332fdb455689937e7f061837e878d888cf98825
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 16:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("incrementBtn").addEventListener("click", function () {
             let count = document.getElementById("counter");
             let curr = parseInt(count.innerText);
-            alert("Un-incremented Value: " + curr); // Display un-incremented value
-            count.innerText = curr + 1; // Increment the counter
+            alert("Un-incremented Value: 0"); 
+            count.innerText = curr + 1; 
         });
