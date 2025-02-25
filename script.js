99a5877b0091cfd1c4348e1892fe9121fc73a8b8
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 16:34:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+document.getElementId("incre").addEventListener("click",function(){
+	let count = document.getElementId("count");
+	let curr= parseInt(count.innerText);
+	alert(curr);
+	count.innerText = curr+1;
+})
